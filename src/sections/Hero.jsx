9d419c7972ef727e39357651a0a49f2e6dc710ef
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2025-08-26 16:48:05 +0530

    hero-complete

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { words } from "../constants";
 import Button from "../components/Button";
+import HeroExperience from "../components/HeroModels/HeroExperience";
 
 const Hero = () => {
     return (
@@ -48,6 +49,11 @@ const Hero = () => {
                     </div>
                 </header>
                 {/* right hero content */}
+                <figure>
+                    <div className="hero-3d-layout">
+                        <HeroExperience />
+                    </div>
+                </figure>
             </div>
         </section>
     );
