ffe5d0d05e6682aa215d9d154b7dda818343c6cd
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2025-08-31 21:27:41 +0530

    about-section

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,7 +1,8 @@
-import { Code2 } from "lucide-react";
+import { BookOpen, Code2, Coffee, Heart, Mail, MapPin } from "lucide-react";
 import qtify from "../assets/qtify.png";
 import netflix_clone from "../assets/netflix-clone.png";
 import youtube_clone from "../assets/youtube-clone.png";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
 
 export const SKILLS_CATEGORY = [
     {
@@ -10,11 +11,11 @@ export const SKILLS_CATEGORY = [
         description: "Crafting beautiful, responsive user interfaces ",
         skills: [
             { name: "JavaScript", level: 80, color: "bg-blue-500" },
-            { name: "React", level: 60, color: "bg-yellow-400" },
-            { name: "Tailwind", level: 75, color: "bg-pink-500" },
-            { name: "Bootstrap", level: 70, color: "bg-purple-500" },
-            { name: "Git", level: 65, color: "bg-cyan-500" },
-            // { name: "HTML", level: 80, color: "bg-green-500" },
+            { name: "React", level: 60, color: "bg-green-400" },
+            { name: "Tailwind", level: 75, color: "bg-purple-500" },
+            { name: "Bootstrap", level: 70, color: "bg-orange-500" },
+            { name: "Git", level: 65, color: "bg-pink-500" },
+            // { name: "HTML", level: 80, color: "bg-cyan-500" },
             // { name: "CSS", level: 70, color: "bg-yellow-900" },
         ],
     },
@@ -54,5 +55,55 @@ export const PROJECTS = [
         liveUrl: "#",
         githubUrl: "#",
         featured: false,
+    },
+];
+
+// export const JOURNEY_STEPS = [{}];
+
+export const PASSIONS = [
+    {
+        icon: Heart,
+        title: "User Experience",
+        description: "Crafting intuitive interfaces",
+    },
+    {
+        icon: Coffee,
+        title: "Problem Solving",
+        description: "Turning challenges into elegant solutions",
+    },
+    {
+        icon: BookOpen,
+        title: "Continuous Learning",
+        description: "Always exploring new technologies and best practices",
+    },
+];
+
+export const SOCIAL_LINKS = [
+    {
+        name: "GitHub",
+        icon: FiGithub,
+        url: "#",
+        color: "hover:text-gray-400",
+        bgColor: "hover:bg-gray-800",
+    },
+    {
+        name: "LinkedIn",
+        icon: FiLinkedin,
+        color: "hover:text-blue-400",
+        bgColor: "hover:bg-blue-500/10",
+    },
+    {
+        name: "Email",
+        icon: Mail,
+        url: "mailto:dev.prasad.kulkarni@gmail.com",
+        color: "hover:text-green-400",
+        bgColor: "hover:bg-green-500/10",
+    },
+];
+
+export const CONTACT_INFO = [
+    {
+        icon: MapPin,
+        label: "Location",
     },
 ];
